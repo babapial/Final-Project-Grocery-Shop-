e73165df08f464ff20f9840245916600142902e6
babapial <pialhasansu@gmail.com>
2024-07-30 18:37:15 +0600

    banner added

--- js/index_js.js
+++ js/index_js.js
@@ -2,7 +2,12 @@ let serachFrom = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>
 {
-    serachFrom.classList.toggle('active');
+    serachFrom.classList.toggle('active');   //active only
+
+    
+    shopingCart.classList.remove('active');
+    logingFrom.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let shopingCart = document.querySelector('.shoping-cart');
@@ -10,6 +15,11 @@ let shopingCart = document.querySelector('.shoping-cart');
 document.querySelector('#cart-btn').onclick = () =>
 {
     shopingCart.classList.toggle('active');
+
+
+    serachFrom.classList.remove('active');
+    logingFrom.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 
@@ -18,6 +28,36 @@ let logingFrom = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () =>
 {
     logingFrom.classList.toggle('active');
+
+
+    serachFrom.classList.remove('active');
+    shopingCart.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+    navbar.classList.toggle('active');
+
+    serachFrom.classList.remove('active');
+    logingFrom.classList.remove('active');
+    shopingCart.classList.remove('active');
+    
+
+    
+}
+
+
+window.onscroll = () =>
+{
+    serachFrom.classList.remove('active');
+    shopingCart.classList.remove('active');
+    logingFrom.classList.remove('active');
+    navbar.classList.remove('active');
+    
+    
+}
 
